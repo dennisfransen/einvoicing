c4179f2efa600ccc3bd7889083a6cee206a5da3c
Dennis Fransén <dennisfransen@hotmail.se>
2025-04-22 14:16:35 +0200

    Deleted false @param phpdocumentation. Removed spacing

--- src/Models/InvoiceTotals.php
+++ src/Models/InvoiceTotals.php
@@ -157,13 +157,11 @@ class InvoiceTotals {
         return $totals;
     }
 
-
     /**
      * Update VAT map
-     * @param array<string,VatBreakdown> &$vatMap          VAT map reference
-     * @param VatTrait                   $item             Item instance
-     * @param float|null                 $rate             VAT rate
-     * @param float                      $addTaxableAmount Taxable amount to add
+     * @param array<string,VatBreakdown> &$vatMap VAT map reference
+     * @param VatTrait $item Item instance
+     * @param float $addTaxableAmount Taxable amount to add
      */
     static private function updateVatMap(array &$vatMap, $item, float $addTaxableAmount) {
         $category = $item->getVatCategory();
